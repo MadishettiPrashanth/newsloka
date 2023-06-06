196ceab6a95b485d7c54278e7666414845f18ac8
MadishettiPrashanth <prashanthmadishetti1999@gmail.com>
2023-06-06 22:42:16 +0530

    hello

---
+++ src/simple/Test.java
@@ -0,0 +1,16 @@
+package simple;
+
+public class Test {
+
+	public void m1()
+	{
+		System.out.println("hello world");
+	}
+	
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		t.m1();
+	}
+
+}
